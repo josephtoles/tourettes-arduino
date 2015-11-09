29f070af4cf2cbd0cf2a2ffce7c8603d2c8e7c0b
josephtoles <jbtoles@gmail.com>
2015-11-09 02:44:21 -0800

    Digital works much better!

--- tourettes.ino
+++ tourettes.ino
@@ -16,7 +16,7 @@ int READY_PIN = 4;
 
 /***************************
  * Bluefruit configuration *
- ***************************/
+ ***************************/                                
 
 #include <Arduino.h>
 #include <SPI.h>
@@ -43,6 +43,16 @@ Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_
 #include "adxl335.h"
 
 
+// ADXL345 headers
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+
+
+
+
 /**************************************************************************/
 /*!
     @brief  Sets up the HW an the BLE module (this function is called
@@ -53,6 +63,7 @@ void setup(void)
 {
   pinMode(READY_PIN, OUTPUT);
   digitalWrite(READY_PIN, LOW);
+  digitalWrite(READY_PIN, HIGH);  //TODO remove
   
   while (!Serial);  // required for Flora & Micro
   delay(500);
@@ -60,7 +71,8 @@ void setup(void)
   Serial.begin(9600);
 
   bluefruit_setup();
-  setup_adxl335();
+  //setup_adxl335();
+  ADXL355_setup();
 }
 
 /**************************************************************************/
@@ -74,4 +86,281 @@ void loop(void)
   loop_adxl1335();
 }
 
+
+/**
+ * Code
+  * TODO move to separate files
+ */
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  accel.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" m/s^2");  
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+
+void displayDataRate(void)
+{
+  Serial.print  ("Data Rate:    "); 
+  
+  switch(accel.getDataRate())
+  {
+    case ADXL345_DATARATE_3200_HZ:
+      Serial.print  ("3200 "); 
+      break;
+    case ADXL345_DATARATE_1600_HZ:
+      Serial.print  ("1600 "); 
+      break;
+    case ADXL345_DATARATE_800_HZ:
+      Serial.print  ("800 "); 
+      break;
+    case ADXL345_DATARATE_400_HZ:
+      Serial.print  ("400 "); 
+      break;
+    case ADXL345_DATARATE_200_HZ:
+      Serial.print  ("200 "); 
+      break;
+    case ADXL345_DATARATE_100_HZ:
+      Serial.print  ("100 "); 
+      break;
+    case ADXL345_DATARATE_50_HZ:
+      Serial.print  ("50 "); 
+      break;
+    case ADXL345_DATARATE_25_HZ:
+      Serial.print  ("25 "); 
+      break;
+    case ADXL345_DATARATE_12_5_HZ:
+      Serial.print  ("12.5 "); 
+      break;
+    case ADXL345_DATARATE_6_25HZ:
+      Serial.print  ("6.25 "); 
+      break;
+    case ADXL345_DATARATE_3_13_HZ:
+      Serial.print  ("3.13 "); 
+      break;
+    case ADXL345_DATARATE_1_56_HZ:
+      Serial.print  ("1.56 "); 
+      break;
+    case ADXL345_DATARATE_0_78_HZ:
+      Serial.print  ("0.78 "); 
+      break;
+    case ADXL345_DATARATE_0_39_HZ:
+      Serial.print  ("0.39 "); 
+      break;
+    case ADXL345_DATARATE_0_20_HZ:
+      Serial.print  ("0.20 "); 
+      break;
+    case ADXL345_DATARATE_0_10_HZ:
+      Serial.print  ("0.10 "); 
+      break;
+    default:
+      Serial.print  ("???? "); 
+      break;
+  }  
+  Serial.println(" Hz");  
+}
+
+
+void displayRange(void)
+{
+  Serial.print  ("Range:         +/- "); 
+  
+  switch(accel.getRange())
+  {
+    case ADXL345_RANGE_16_G:
+      Serial.print  ("16 "); 
+      break;
+    case ADXL345_RANGE_8_G:
+      Serial.print  ("8 "); 
+      break;
+    case ADXL345_RANGE_4_G:
+      Serial.print  ("4 "); 
+      break;
+    case ADXL345_RANGE_2_G:
+      Serial.print  ("2 "); 
+      break;
+    default:
+      Serial.print  ("?? "); 
+      break;
+  }  
+  Serial.println(" g");  
+}
+
+
+// A small helper (Bluefruit)
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+
+void ADXL355_setup(void) {
+  Serial.println("Accelerometer Test"); Serial.println("");
+  
+  // Initialise the sensor
+  if(!accel.begin())
+  {
+    // There was a problem detecting the ADXL345 ... check your connections
+    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    while(1);
+  }
+
+  // Set the range to whatever is appropriate for your project
+  accel.setRange(ADXL345_RANGE_16_G);
+  // displaySetRange(ADXL345_RANGE_8_G);
+  // displaySetRange(ADXL345_RANGE_4_G);
+  // displaySetRange(ADXL345_RANGE_2_G);
+  
+  // Display some basic information on this sensor
+  displaySensorDetails();
+  
+  // Display additional settings (outside the scope of sensor_t)
+  displayDataRate();
+  displayRange();
+  Serial.println("");
+}
+
+
+void bluefruit_setup() {
+  Serial.println(F("Adafruit Bluefruit Command Mode Example"));
+  Serial.println(F("---------------------------------------"));
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  /* Perform a factory reset to make sure everything is in a known state */
+  Serial.println(F("Performing a factory reset: "));
+  if (! ble.factoryReset() ){
+       error(F("Couldn't factory reset"));
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
+  Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  digitalWrite(READY_PIN, HIGH);
+
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+      delay(500);
+  }
+
+  Serial.println(F("*****************"));
+}
+
+
+/**************************************************************************/
+/*!
+    @brief  Checks for user input (via the Serial Monitor)
+*/
+/**************************************************************************/
+bool getUserInput(char buffer[], uint8_t maxSize)
+{
+  // timeout in 100 milliseconds
+  TimeoutTimer timeout(100);
+
+  memset(buffer, 0, maxSize);
+  while( (Serial.peek() < 0) && !timeout.expired() ) {}
+
+  if ( timeout.expired() ) return false;
+
+  delay(2);
+  uint8_t count=0;
+  do
+  {
+    count += Serial.readBytes(buffer+count, maxSize);
+    delay(2);
+  } while( (count < maxSize) && !(Serial.peek() < 0) );
+
+  return true;
+}
+
+
+void bluefruit_loop() {
+
+  /* Get a new sensor event */ 
+  sensors_event_t event; 
+  accel.getEvent(&event);
+ 
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("X: "); Serial.print((int) (10 * event.acceleration.x + 200 + 0.5)); Serial.print("  ");
+  Serial.print("Y: "); Serial.print((int) (10 * event.acceleration.y + 200 + 0.5)); Serial.print("  ");
+  Serial.print("Z: "); Serial.print((int) (10 * event.acceleration.z + 200 + 0.5)); Serial.print("  ");Serial.println("m/s^2 ");
+  //delay(500);
+
+  //  Send arduino --> phone
+  /*
+  // Check for user input
+  char inputs[BUFSIZE+1];
+  if ( getUserInput(inputs, BUFSIZE) )
+  {
+    // Send characters to Bluefruit
+    Serial.print("[Send] ");
+    Serial.println(inputs);
+
+    ble.print("AT+BLEUARTTX=");
+    ble.println(inputs);
+
+    // check response stastus
+    if (! ble.waitForOK() ) {
+      Serial.println(F("Failed to send?"));
+    }
+  }
+  */
+
+  //int x = analogRead(1);
+  //Serial.print("[Send] ");Serial.println(x);
+  ble.print("AT+BLEUARTTX=");ble.print((int) (10 * event.acceleration.x + 200 + 0.5));ble.println("x");
+  delay(50);
+  if (! ble.waitForOK() ) {
+     Serial.println(F("Failed to send x?"));
+  }
+  
+  //int y = analogRead(2);
+  //Serial.print("[Send] ");Serial.println(y);
+  ble.print("AT+BLEUARTTX=");ble.print((int) (10 * event.acceleration.y + 200 + 0.5));ble.println("y");
+  delay(50);
+  if (! ble.waitForOK() ) {
+     Serial.println(F("Failed to send y?"));
+  }
+
+  //int z = analogRead(3);
+  //Serial.print("[Send] ");Serial.println(z);
+  ble.print("AT+BLEUARTTX=");ble.print((int) (10 * event.acceleration.z + 200 + 0.5));ble.println("z");
+  delay(50);
+  if (! ble.waitForOK() ) {
+     Serial.println(F("Failed to send z?"));
+  }
+
+  Serial.print("[Send] ");Serial.println("----");
+  ble.print("AT+BLEUARTTX=");ble.println("----");
+  delay(50);
+  if (! ble.waitForOK() ) {
+     Serial.println(F("Failed to send?"));
+  }
+
+}
 
